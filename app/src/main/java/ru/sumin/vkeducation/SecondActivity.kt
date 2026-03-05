1900a4d14dc954c8ffaa3579a580fb4f56e11d42
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2026-03-05 22:22:52 +0300

    VKEdu: Подключил SecondScreen в SecondActivity и чтение текста через Contract

--- app/src/main/java/ru/sumin/vkeducation/SecondActivity.kt
+++ app/src/main/java/ru/sumin/vkeducation/SecondActivity.kt
@@ -2,9 +2,18 @@ package ru.sumin.vkeducation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import ru.sumin.vkeducation.screen.SecondScreen
+import ru.sumin.vkeducation.ui.theme.VKEducationTheme
 
 class SecondActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val receivedText = Contract.readText(intent = intent)
+        setContent {
+            VKEducationTheme {
+                SecondScreen(receivedText)
+            }
+        }
     }
 }
