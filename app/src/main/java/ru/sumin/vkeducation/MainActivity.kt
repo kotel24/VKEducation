7a0a951306b02fff7d1e365d4ecaf86e61f4471b
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2026-03-05 22:19:43 +0300

    VKEdu: добавил запуск SecondActivity через явный Intent с передачей текста

--- app/src/main/java/ru/sumin/vkeducation/MainActivity.kt
+++ app/src/main/java/ru/sumin/vkeducation/MainActivity.kt
@@ -4,44 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import ru.sumin.vkeducation.screen.MainScreen
 import ru.sumin.vkeducation.ui.theme.VKEducationTheme
 
 class MainActivity : ComponentActivity() {
+
+    private var text by mutableStateOf("")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             VKEducationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen(
+                    text = text,
+                    onTextChanged = {text = it},
+                    onOpenSecond = {
+                        val intent = Contract.createIntent(this, it)
+                        startActivity(intent)
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VKEducationTheme {
-        Greeting("Android")
     }
 }
