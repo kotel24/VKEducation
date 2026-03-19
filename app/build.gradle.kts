86fc4b8d2b69af2bd730250c208772ca45fc0014
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2026-03-19 18:19:19 +0300

    Vkedu: добавил зависимости для hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
@@ -60,6 +62,10 @@ dependencies {
 
     implementation(libs.coil.compose)
     implementation(libs.coil.network.okhttp)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
 
     implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.androidx.navigation.compose)
