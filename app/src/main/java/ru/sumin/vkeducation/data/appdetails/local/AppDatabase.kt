dcbb70bbc4e6464c9e250098bad06f32728dcf8c
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2026-03-26 15:54:20 +0300

    Vkedu: скопировал себе код из коммита «Работа с БД» из директории «data/appdetails/local»

---
+++ app/src/main/java/ru/sumin/vkeducation/data/appdetails/local/AppDatabase.kt
@@ -0,0 +1,19 @@
+package ru.sumin.vkeducation.data.appdetails.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [AppDetailsEntity::class],
+    version = 1,
+)
+@TypeConverters(CategoryConverter::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun appDetailsDao(): AppDetailsDao
+
+    companion object {
+        const val DATABASE_NAME = "app_database"
+    }
+
+}
